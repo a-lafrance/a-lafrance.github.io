df682099e2a82c7fc1be767dacb48f504b0894d0
Arthur Lafrance <lafrancearthur@gmail.com>
2021-02-12 11:41:14 -0800

    vue project setup

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <List :items="this.items" />
+</template>
+
+<script>
+import List from './components/List.vue'
+
+export default {
+  name: 'App',
+  components: {
+    List
+  },
+  data: function() {
+    return {
+      items: [
+        { id: 1, text: 'hello, world' },
+        { id: 2, text: 'asg' },
+        { id: 3, text: 'acya' },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="list">
+    <li class="list-item" v-for="item in items" :key="item.id">
+      {{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    items: Array
+  }
+}
+</script>
+
+<style scoped>
+.entry-list {
+  padding-left: 2vh;
+}
+
+.list-item {
+  font-size: 11pt;
+}
+</style>
