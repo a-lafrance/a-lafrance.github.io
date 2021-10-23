206bfd1a79f501db65abc8cf10078ff55275b02c
Arthur Lafrance <lafrancearthur@gmail.com>
2021-10-23 02:37:44 -0700

    mid-f21 content refresh

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@
         <h3 class="navy">I'm Arthur Lafrance</h3>
 
         <div id="bottom">
-          <p>Although I was born and raised in Silicon Valley, I currently study computer science
-            at the UC Irvine Bren School of ICS, as a member of the class of 2023. I'm very broadly interested in
-            compilers, programming language design, &amp; computer architecture.
+          <p>Born &amp; raised in Silicon Valley; currently studying computer science at UC Irvine, class of 2023. Also
+             in between summers at Facebook. I'm broadly interested in compilers, programming language design, &amp; computer
+             architecture.
           </p>
 
           <div id="contact">
@@ -28,12 +28,10 @@
   </Section>
 
   <EntryList name="current-work" title="Things I'm Working On" :entries="currentWork">
-    <p class="description">A short portfolio of cool stuff I'm currently building</p>
+      <p class="description">Given the volatile nature of my projects at this point, I'm sure this list will be in a pretty constant state of flux.</p>
   </EntryList>
 
-  <EntryList name="past-work" title="Things I've Worked On" :entries="pastWork">
-    <p class="description">Some interesting projects I worked on in the past</p>
-  </EntryList>
+  <EntryList name="past-work" title="Things I've Worked On" :entries="pastWork" />
 
   <Section name="resume-dump" title="Resume Dump">
     <p class="description">A giant wall of text more or less regurgitated from my resume</p>
@@ -77,13 +75,20 @@ export default {
       ],
       currentWork: [
         {
-          title: "Pensiv",
-          subtitles: [],
-          description: `My interests in Rust and AI/ML coincided to create Pensiv: my attempt at a Rust-based AI/ML library. I felt
-          like it would be a great way to get experience in both domains while exploring an area that's not oversaturated (see: the thousands of Python AI libraries).
-          It's an interesting technical challenge to design idiomatic Rust interfaces for AI/ML concepts while also trying not to get locked in a death match with the
-          borrow checker. So far I've gotten some basic probability & adversarial search functionality done, with more to come in the future.`,
-          link: "https://www.github.com/arthurlafrance/Pensiv"
+            title: "lfc",
+            subtitles: [],
+            description: `To be brutally honest, C doesn't give you much out of the box. That's part of the reason I enjoy using it, but I can already
+            foresee that it'll be annoying to write the same set of utilities 50 different times per-project. So, lfc is my personal solution to that problem, point
+            being to basically fill any niche I need that the C (and POSIX I guess) standard library doesn't adequately provide. Some days it's really cool, and
+            others it feels like I'm writing the code equivalent of bitter medicine, where I just have to finish it because I know it's good for me.`
+        },
+        {
+            title: "Mustang",
+            subtitles: [],
+            description: `Recently I've been experimenting with the idea of writing a C toolchain (in C of course). I figure it'll probably be useful when I
+            start writing more and more C projects, because who wants to set up the same Makefile and directory structure and manually build everything all the time. Instead,
+            it seems more fun to write the stuff I need myself, much the same as with lfc. Who knows whether or not it'll pan out or turn out as useful as it seems, but it's
+            worth a shot.`
         },
         {
           title: "???",
@@ -100,12 +105,26 @@ export default {
           when we decided to create a website that could dynamically generate these "Pattis diagrams" for arbitrary Python code. We got a decent proof-of-concept
           going at the hackathon, for which we were fortunate enough to win Best Entrepreneurial Hack, and we then spent the next 6 months or so fully bringing
           the idea to life. We went through several full rewrites of the app, learned a ton about Python, Django, and software design, and eventually did end up with
-          a functioning website that did what we originally intended. To date I think it's the most complex piece of software I've built from scratch, so needless to say
-          I had a good time technically and personally building it with my team.`, // TODO: insert links to ryan/patrick sites
+          a functional website that did what we originally intended. To date I think it's the most complex piece of software I've built from scratch, so needless to say
+          I had a great technical and personal experience building it with my team.`, // TODO: insert links to ryan/patrick sites
           link: "https://www.github.com/tapestrylearn/Diagrammer"
         }
       ],
       resumeDump: [
+        {
+            text: "Lab Tutor, UC Irvine, September 2021 - present",
+            subitems: [
+                `Working as an ICS lab tutor this fall, I've been supporting students in ICS 53 (Principles in System Design) by supervising open lab hours.
+                On a daily basis, I help students understand course concepts, debug their systems-level C programs, & gain proficiency with essential Unix tools like GDB.`
+            ]
+        },
+        {
+            text: "Undergraduate Researcher, UC Irvine, September 2021 - present",
+            subitems: [
+                `During Fall 2021, I've been working on network databases & graph alignment with Prof. Wayne Hayes. My group is mainly concerned with
+                developing ways to find good seeds for graph alignment, mainly useful for applications in computational biology.`
+            ]
+        },
         {
           text: "Software Engineering Intern, Facebook, June 2021 - September 2021",
           subitems: [
@@ -119,8 +138,8 @@ export default {
           text: "Software Developer, UCI Student Center & Event Services, February 2020 - present",
           subitems: [
             `At the Student Center I've gotten a chance to work on some pretty cool apps (UCI students might recognize ZotFinder & UCI Now among them) and
-            grow significantly as a developer. I was originally hired as an iOS developer, working full-stack with Swift & Python/AWS; recently I've been
-            transitioning into a web development role as well, so I've been picking up C# in addition to my work on iOS.`
+            grow significantly as a developer. As a full-stack iOS developer, I've worked with Swift, Python, AWS, & MySQL. I've really enjoyed getting the
+            chance to make an impact on the campus community through my work.`
           ]
         },
         {
