f9263f59bdf98d0d03d96201a93afcd827d4d3ca
Arthur Lafrance <lafrancearthur@gmail.com>
2022-04-18 01:51:22 -0700

    deploy: project content refresh

--- index-13d57ec0916b2d34.js
+++ index-13d57ec0916b2d34.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-ee8ee3246629bd95_bg.wasm', import.meta.url);
+        input = new URL('index-13d57ec0916b2d34_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -474,7 +474,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper437 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper438 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 176, __wbg_adapter_18);
         return addHeapObject(ret);
     };
