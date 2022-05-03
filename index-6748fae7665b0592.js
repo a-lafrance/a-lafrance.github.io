c188b89c518230034b84b21bcc509948c37c2d16
Arthur Lafrance <lafrancearthur@gmail.com>
2022-05-03 00:44:27 -0700

    deploy: content nuke

--- index-6748fae7665b0592.js
+++ index-6748fae7665b0592.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-b1be4f09819c15fb_bg.wasm', import.meta.url);
+        input = new URL('index-6748fae7665b0592_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -474,8 +474,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper438 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 176, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper390 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 145, __wbg_adapter_18);
         return addHeapObject(ret);
     };
 
