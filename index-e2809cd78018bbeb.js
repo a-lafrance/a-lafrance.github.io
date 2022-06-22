4d496da5741b461f6abed8aa6e11592114462b74
Arthur Lafrance <lafrancearthur@gmail.com>
2022-06-22 00:13:07 -0700

    deploy: update for meta s22

--- index-e2809cd78018bbeb.js
+++ index-e2809cd78018bbeb.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-8ee157ae5d592c2c_bg.wasm', import.meta.url);
+        input = new URL('index-e2809cd78018bbeb_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
