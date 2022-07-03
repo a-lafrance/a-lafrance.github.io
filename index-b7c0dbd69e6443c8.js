b0aa97e1bc9addce149a2edf75aea19d0e6fe4cd
Arthur Lafrance <lafrancearthur@gmail.com>
2022-07-03 03:16:59 -0700

    deploy: various wording/content changes

--- index-b7c0dbd69e6443c8.js
+++ index-b7c0dbd69e6443c8.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-e2809cd78018bbeb_bg.wasm', import.meta.url);
+        input = new URL('index-b7c0dbd69e6443c8_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
