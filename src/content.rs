dc00834a0d98e43018b208fdf1146a7cee0629ff
Arthur Lafrance <lafrancearthur@gmail.com>
2023-11-03 21:35:09 -0700

    deploy: meta

--- src/content.rs
+++ src/content.rs
@@ -16,7 +16,7 @@ impl Content {
                 BioLink { name: "LinkedIn", link: "https://linkedin.com/in/a-lafrance" },
             ],
             current_work: vec![
-                Item::from("Returning to Meta to work on polyglot FFI compiler infra (again)"),
+                Item::from("Working on polyglot FFI compiler infra (again) at Meta"),
                 Item::from("Trying to contribute to Rust more regularly"),
                 Item(vec![
                     Token::Media("Writing about music", "https://instagram.com/goodvibrations._"),
