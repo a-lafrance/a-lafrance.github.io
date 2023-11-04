ab28f0296a2477f3b1d45e82eeaecff54a81ff17
Arthur Lafrance <lafrancearthur@gmail.com>
2023-11-04 01:33:24 -0700

    deploy: change plos

--- index-864ceb1d8a00d478.js
+++ index-864ceb1d8a00d478.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-c4d3b3f65fd5c20f_bg.wasm', import.meta.url);
+        input = new URL('index-864ceb1d8a00d478_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
